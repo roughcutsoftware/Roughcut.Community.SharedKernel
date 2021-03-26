7513b9e182702fba64b9deaf387e4727bbdeecf0
Houston <community@roughcutsoftware.com>
2021-03-26 09:41:54 -0700

    added in web project for use within Azure DevOps

---
+++ playground/Roughcut.Community.SharedKernel.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Roughcut.Community.SharedKernel.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- tests/Roughcut.Community.SharedKernel.Tests/UnitTests/SharedKernelTests.cs
+++ tests/Roughcut.Community.SharedKernel.Tests/UnitTests/SharedKernelTests.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using NUnit.Framework;
+using Roughcut.Community.SharedKernel.Web.Controllers;
 using Shouldly;
 
 namespace Roughcut.Community.SharedKernel.Tests.UnitTests
@@ -11,7 +15,7 @@ namespace Roughcut.Community.SharedKernel.Tests.UnitTests
         }
 
         [TestCase("This works!...")]
-        public void Should_BeAFailedTest(string stringToAssert)
+        public void Should_BeFailedTest(string stringToAssert)
         {
             // arrange / act
             string failingTestString = DefaultTestCase.FailedTest();
@@ -21,13 +25,28 @@ namespace Roughcut.Community.SharedKernel.Tests.UnitTests
         }
 
         [TestCase("This works!...")]
-        public void Should_BeASuccessfulTest(string stringToAssert)
+        public void Should_BeSuccessfulTest(string stringToAssert)
         {
             // arrange / act
             string successfulTest = DefaultTestCase.SuccessfulTest();
 
             // assert
             successfulTest.ShouldBe(stringToAssert);
+        }
+
+        [TestCase("This works!...")]
+        public void Should_BeSuccessfulWebTest(string stringToAssert)
+        {
+            // arrange
+            HomeController controller = new HomeController(new Logger<HomeController>(new NullLoggerFactory()));
+
+            // act
+            IActionResult result = controller.Index() as IActionResult;
+
+            // assert
+            result.ShouldNotBeNull();
+
+  
         }
     }
 }
